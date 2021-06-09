7902957a45d241260f0640c54e984f711f8c0f76
Prince0047 <sonubhadana838@gmail.com>
2021-06-09 13:21:02 +0530

    profile and comments componenets added

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,9 @@
-
-import { Fragment, useEffect } from 'react';
-
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
 import Navbar from './components/layout/Navbar';
-import {Landing} from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import Alert from './components/layout/Alert';
-import Dashboard from './components/dashboard/Dashboard';
-import PrivateRoute from './components/routing/PrivateRoute';
+import Landing from './components/layout/Landing';
+import Routes from './components/routing/Routes';
+import { LOGOUT } from './actions/types';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -19,33 +13,33 @@ import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
 
-if(localStorage.token) {
-  setAuthToken(localStorage.token);
-}
-
 const App = () => {
-
   useEffect(() => {
+    // check for token in LS
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
     store.dispatch(loadUser());
+
+    // log user out from all tabs if they log out in one tab
+    window.addEventListener('storage', () => {
+      if (!localStorage.token) store.dispatch({ type: LOGOUT });
+    });
   }, []);
 
   return (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component= {Landing} />
-        <section className="container">
-          <Alert />
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
           <Switch>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-            <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/" component={Landing} />
+            <Route component={Routes} />
           </Switch>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-)};
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
